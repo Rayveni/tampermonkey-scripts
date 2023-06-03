8123f51ee21919aa01393a07d9c5ca6c104b1588
I.V <is.volochkov@gmail.com>
2023-06-04 01:43:07 +0300

    fix

--- add buttons/add_btn.js
+++ add buttons/add_btn.js
@@ -4,60 +4,61 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        http://asust.test-01.station.tsft.stdev.ru/*
+// @match       
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=stdev.ru
 // @run-at document-end
 // ==/UserScript==
 
-const _timeout=1000,_interval=200;
-var prev_res=false;
-function heart_beat(_f){
+const _timeout = 1000,
+  _interval = 200;
+var prev_res = false;
 
-	let timerId = setInterval(function() {
-    let res=check_selector();
-	    if (res){
-			if (!prev_res){
-				let _element=document.querySelector('.tsft-modal__top'),
-				ccs_params={ 'z-index': 3, backgroundColor: 'red'};
-				addButton('favicons', addButton,ccs_params,_element);
-
-
-			prev_res=true;
-			}
-		 clearInterval(timerId);
-	}else
-	{
-		prev_res=false;
-	}
-
-}, _interval);
-
-// остановить вывод через 5 секунд
-setTimeout(() => { clearInterval(timerId);}, _timeout);
-}
-
-function check_selector(){
-	let res=false,
-        elems = document.querySelectorAll('.arrival-reg');
-    if(elems.length > 0){
-        //document.querySelector('.tsft-modal__win').style.backgroundColor='red'
-        res=true;
-    }
-	return res
-}
-
-addEventListener("mousedown", (event) => {
-heart_beat(check_selector);
+addEventListener('mousedown', (event) => {
+  heart_beat(check_selector);
 });
 
-    function addButton(text, onclick,ccs_params,_selector, cssObj) {
-		let button = document.createElement('button'), btnStyle = button.style;
-		   // ccs_params={position: 'absolute', top: '9%', left:'4%', 'z-index': 3, backgroundColor: 'red'};
-        cssObj = cssObj || ccs_params;
-
-        _selector.prepend(button);
-        button.innerHTML = text;
-        button.onclick = onclick;
-        Object.keys(cssObj).forEach(key => btnStyle[key] = cssObj[key]);
+function heart_beat(_f) {
+  let timerId = setInterval(function () {
+    let res = check_selector();
+    if (res) {
+      if (!prev_res) {
+        let _element = document.querySelector('.tsft-modal__top'),
+          ccs_params = {
+            'z-index': 3,
+            backgroundColor: 'red',
+          };
+        addButton('favicons', addButton, ccs_params, _element);
+        prev_res = true;
+      }
+      clearInterval(timerId);
+    } else {
+      prev_res = false;
     }
+  }, _interval);
 
+  // остановить вывод через 5 секунд
+  setTimeout(() => {
+    clearInterval(timerId);
+  }, _timeout);
+}
+
+function check_selector() {
+  let res = false,
+    elems = document.querySelectorAll('.arrival-reg');
+  if (elems.length > 0) {
+    res = true;
+  }
+  return res;
+}
+
+function addButton(text, onclick, ccs_params, _selector, cssObj) {
+  let button = document.createElement('button'),
+    btnStyle = button.style,
+    default_css_params={backgroundColor: 'red'};
+  // ccs_params={position: 'absolute', top: '9%', left:'4%', 'z-index': 3, backgroundColor: 'red'};
+  cssObj = cssObj || ccs_params;
+  _selector.prepend(button);
+  button.innerHTML = text;
+  button.onclick = onclick;
+  Object.keys(cssObj).forEach((key) => (btnStyle[key] = cssObj[key]));
+}
